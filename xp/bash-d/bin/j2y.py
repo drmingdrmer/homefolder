d701c49b626945106013bc270cc935f58d2c6742
drdr xp <drdr.xp@gmail.com>
2015-10-25 22:12:52 +0800

    add j2y to convert json to yaml

---
+++ xp/bash-d/bin/j2y.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# convert json to yaml
+
+import sys
+
+import json
+import yaml
+
+if len( sys.argv ) > 1:
+    fn = sys.argv[1]
+    with open(fn) as f:
+        cont = f.read()
+else:
+    cont = sys.stdin.read()
+
+datas = json.loads(cont, encoding='utf-8')
+
+print yaml.safe_dump(datas,
+                     encoding='utf-8',
+                     allow_unicode=True,
+                     # default_flow_style=False
+)
