ad9e36b535c7e8b6404e121c24db8dd070519a5d
drdr xp <drdr.xp@gmail.com>
2015-12-10 02:15:26 +0000

    fix crc32

--- xp/bash-d/bin/crc32.py
+++ xp/bash-d/bin/crc32.py
@@ -9,7 +9,9 @@ if len( sys.argv ) > 1:
     with open(fn) as f:
         cont = f.read()
 else:
-    cont = sys.stdin.read()
+    cont = sys.stdin.read().strip()
 
 crc32 = binascii.crc32( cont )
+crc32 = crc32 % 0xffffffff
+print crc32
 print '%08x' % crc32
