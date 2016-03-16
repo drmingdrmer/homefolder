9c639b656151dc64820dbbb493d8b62899463dc7
drdr xp <drdr.xp@gmail.com>
2016-03-16 11:22:05 +0800

    pust to cheatsheet

--- xp/bash-d/script/subtree-project-update.sh
+++ xp/bash-d/script/subtree-project-update.sh
@@ -3,3 +3,7 @@
 cd $(git rev-parse --show-toplevel)
 
 git subtree split -P xp/bash-d/plugin/cheatsheet --branch cheatsheet --rejoin
+
+git remote add cs git@github.com:drmingdrmer/cheatsheet.git
+
+git push cs cheatsheet:master
