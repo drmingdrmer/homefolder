40864bcef99bcf957812be5694bd521ea26a9982
drdr xp <drdr.xp@gmail.com>
2016-05-18 19:53:13 +0800

    remove remote after subproject pushed

--- xp/bash-d/script/subtree-project-update.sh
+++ xp/bash-d/script/subtree-project-update.sh
@@ -7,3 +7,5 @@ git subtree split -P xp/bash-d/plugin/cheatsheet --branch cheatsheet --rejoin
 git remote add cs git@github.com:drmingdrmer/cheatsheet.git
 
 git push cs cheatsheet:master
+
+git remote remove cs
