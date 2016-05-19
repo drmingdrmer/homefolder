dbf4aed7b2cb08b8a7b108048c39d95c763d26b0
drdr xp <drdr.xp@gmail.com>
2016-05-19 17:43:08 +0800

    do not add remote for subtree update

--- xp/bash-d/script/subtree-project-update.sh
+++ xp/bash-d/script/subtree-project-update.sh
@@ -4,8 +4,4 @@ cd $(git rev-parse --show-toplevel)
 
 git subtree split -P xp/bash-d/plugin/cheatsheet --branch cheatsheet --rejoin
 
-git remote add cs git@github.com:drmingdrmer/cheatsheet.git
-
-git push cs cheatsheet:master
-
-git remote remove cs
+git push git@github.com:drmingdrmer/cheatsheet.git cheatsheet:master
