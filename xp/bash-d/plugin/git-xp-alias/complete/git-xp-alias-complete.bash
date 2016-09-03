33c130039eaec95ad2ea074a3253da2e2bcecedd
drdr xp <drdr.xp@gmail.com>
2016-09-02 21:04:18 -0700

    add complete for gh

--- xp/bash-d/plugin/git-xp-alias/complete/git-xp-alias-complete.bash
+++ xp/bash-d/plugin/git-xp-alias/complete/git-xp-alias-complete.bash
@@ -3,7 +3,8 @@
 _git_l() { _git_log; }
 _git_rm_branch() { _git_branch; }
 
-__git_complete gl _git_log
+__git_complete gl  _git_log
 __git_complete gll _git_log
 __git_complete gld _git_log
+__git_complete gh  _git_log
 
