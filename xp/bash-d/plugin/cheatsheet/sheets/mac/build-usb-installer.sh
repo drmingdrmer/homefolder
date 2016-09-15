42971628f931069b260fc473ab1c243c68246d13
drdr xp <drdr.xp@gmail.com>
2016-09-15 16:19:42 +0800

    add build-usb-installer

---
+++ xp/bash-d/plugin/cheatsheet/sheets/mac/build-usb-installer.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo /Applications/Install\ OS\ X\ El\ Capitan.app/Contents/Resources/createinstallmedia --volume /Volumes/Untitled --applicationpath /Applications/Install\ OS\ X\ El\ Capitan.app --nointeraction
