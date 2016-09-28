22c44626524eba8f160886e8b5bcd605e7a0c987
drdr xp <drdr.xp@gmail.com>
2016-09-28 18:47:44 +0800

    remove useless

--- xp/bash-d/plugin/git-xp-alias/complete/git-xp-alias-complete.bash
+++ xp/bash-d/plugin/git-xp-alias/complete/git-xp-alias-complete.bash
@@ -6,8 +6,6 @@ _git_rm_branch() { _git_branch; }
 __git_complete ,d  _git_diff
 __git_complete gl  _git_log
 __git_complete gll _git_log
-__git_complete gld _git_log
-__git_complete gh  _git_log
 
 _git_comma()
 {
