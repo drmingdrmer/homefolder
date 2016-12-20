a61f094b4f8ffaef3bf69ef6ecf1aee7f4181162
drdr xp <drdr.xp@gmail.com>
2016-12-20 19:46:56 +0800

    A  xp/bash-d/plugin/cheatsheet/sheets/pip/use-another-source.sh

---
+++ xp/bash-d/plugin/cheatsheet/sheets/pip/use-another-source.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# http://pypi.douban.com/  豆瓣
+# http://pypi.hustunique.com/  华中理工大学
+# http://pypi.sdutlinux.org/  山东理工大学
+# http://pypi.mirrors.ustc.edu.cn/  中国科学技术大学
+
+pip install web.py -i http://pypi.douban.com/simple
+
+
+# ~/.pip/pip.conf
+# [global]
+# index-url = http://pypi.douban.com/simple
