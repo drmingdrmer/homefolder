d2ffa5a0b288cae8ddfc8e1fc4bd8551ce63d9ca
drdr xp <drdr.xp@gmail.com>
2017-03-11 17:15:47 +0800

    adaptive font-size

--- xp/bash-d/bin/make-coding-logo.sh
+++ xp/bash-d/bin/make-coding-logo.sh
@@ -4,5 +4,13 @@ name=$2
 
 convert -define jpeg:size=200x200 "$1"  -thumbnail 400x400^ -gravity center -extent 400x400 tmp.gif
 
-convert tmp.gif  -weight 700  -pointsize 80  -fill black -draw "rectangle 0,320 400,400" -draw "gravity north fill white text 0,320 '$name' " "$1.logo.gif"
+max_width=720
+l=${#name}
+if [ $l -le 6 ]; then
+    fontsize=120
+else
+    let fontsize=720/l
+fi
+
+convert tmp.gif  -weight 700  -pointsize $fontsize  -fill black -draw "rectangle 0,250 400,400" -draw "gravity north fill white text 0,250 '$name' " "$1.logo.gif"
 
