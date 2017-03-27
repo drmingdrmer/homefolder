905d2acab68143275f32f9983579f8230da707ba
drdrxp <drdrxp@drdrxpdeMacBook-Pro.local>
2017-03-27 20:54:34 +0800

    M  xp/bash-d/plugin/git-multiplex/bin/git-subrepo

---
+++ xp/bash-d/plugin/git-multiplex/update-remote.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+(
+cd src \
+    && make
+) || exit 1
+ 
+(
+cd gist \
+    && ./gist-ctrl commit \
+    && ./gist-ctrl push
+) || exit 1
+
+(
+cd repo \
+    && ./ctl commit \
+    && ./ctl push
+) || exit 1
