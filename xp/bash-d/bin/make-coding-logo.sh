f19762ec4dfe19b9944a2e636cfd9ec93e713037
drdrxp <drdrxp@drdrxpdeMacBook-Pro.local>
2017-03-28 19:36:08 +0800

    M  xp/bash-d/bin/make-coding-logo.sh

--- xp/bash-d/bin/make-coding-logo.sh
+++ xp/bash-d/bin/make-coding-logo.sh
@@ -4,7 +4,10 @@ name=$2
 
 convert -define jpeg:size=200x200 "$1"  -thumbnail 400x400^ -gravity center -extent 400x400 tmp.gif
 
+box_height=150
+let box_top=400-box_height
 max_width=720
+
 l=${#name}
 if [ $l -le 6 ]; then
     fontsize=120
@@ -12,5 +15,13 @@ else
     let fontsize=720/l
 fi
 
-convert tmp.gif  -weight 700  -pointsize $fontsize  -fill black -draw "rectangle 0,250 400,400" -draw "gravity north fill white text 0,250 '$name' " "$1.logo.gif"
+let text_top=400-box_height/2-fontsize/2
+
+convert tmp.gif \
+    -weight 700 \
+    -pointsize $fontsize  \
+    -fill black \
+    -draw "rectangle 0,$box_top 400,400" \
+    -draw "gravity north fill white text 0,$text_top '$name'" \
+    "$1.logo.gif"
 
