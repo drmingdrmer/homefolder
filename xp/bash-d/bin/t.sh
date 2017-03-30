a3fd16b5eeab69aca1674b98e12585c79e27190e
drdrxp <drdrxp@drdrxpdeMacBook-Pro.local>
2017-03-30 15:44:16 +0800

    M  xp/bash-d/bin/t.sh

--- xp/bash-d/bin/t.sh
+++ xp/bash-d/bin/t.sh
@@ -3,11 +3,6 @@
 if [ $# -eq 0 ]; then
     python2 -m unittest discover -v --failfast
 else
-    if [ -d "$1" ]; then
-        # find test from a subdir
-        python2 -m unittest discover -v --failfast -s "$1"
-    else
-        # find test by module[.Class[.function]]
-        python2 -m unittest -v --failfast "$1"
-    fi
+    # find test from a subdir or a module
+    python2 -m unittest discover -v --failfast -s "$1"
 fi
