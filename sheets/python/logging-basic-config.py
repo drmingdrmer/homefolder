0e7ef3ad5e96216266ead0c7b8020b30788fcde0
drdr xp <drdr.xp@gmail.com>
2017-07-08 10:39:03 +0800

    A  xp/bash-d/plugin/cheatsheet/sheets/python/logging-basic-config.py

---
+++ sheets/python/logging-basic-config.py
@@ -0,0 +1,13 @@
+
+# filename  Specifies that a FileHandler be created, using the specified
+#           filename, rather than a StreamHandler.
+# filemode  Specifies the mode to open the file, if filename is specified
+#           (if filemode is unspecified, it defaults to 'a').
+# format    Use the specified format string for the handler.
+# datefmt   Use the specified date/time format.
+# level     Set the root logger level to the specified level.
+# stream    Use the specified stream to initialize the StreamHandler. Note
+#           that this argument is incompatible with 'filename' - if both
+#           are present, 'stream' is ignored.
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
