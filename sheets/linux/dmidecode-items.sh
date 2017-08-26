3e356c36c16bbaa35ee7ebd40f497b41886d938a
drdr xp <drdr.xp@gmail.com>
2017-08-26 16:32:46 +0800

    M  .shbookmark

---
+++ sheets/linux/dmidecode-items.sh
@@ -0,0 +1,26 @@
+# dmidecode -s system-serial-number
+
+
+dmidecode -s bios-vendor
+dmidecode -s bios-version
+dmidecode -s bios-release-date
+dmidecode -s system-manufacturer
+dmidecode -s system-product-name
+dmidecode -s system-version
+dmidecode -s system-serial-number
+dmidecode -s system-uuid
+dmidecode -s baseboard-manufacturer
+dmidecode -s baseboard-product-name
+dmidecode -s baseboard-version
+dmidecode -s baseboard-serial-number
+dmidecode -s baseboard-asset-tag
+dmidecode -s chassis-manufacturer
+dmidecode -s chassis-type
+dmidecode -s chassis-version
+dmidecode -s chassis-serial-number
+dmidecode -s chassis-asset-tag
+dmidecode -s processor-family
+dmidecode -s processor-manufacturer
+dmidecode -s processor-version
+dmidecode -s processor-frequency
+
