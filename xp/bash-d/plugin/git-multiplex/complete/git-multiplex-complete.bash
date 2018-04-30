f633992f1b5915865079d4ab62da43b9bb84091e
张炎泼 <xp@baishancloud.com>
2018-04-30 13:19:29 +0800

    refine git-multiplex command complete, by reseting "$words[1]"(the sub command)

--- xp/bash-d/plugin/git-multiplex/complete/git-multiplex-complete.bash
+++ xp/bash-d/plugin/git-multiplex/complete/git-multiplex-complete.bash
@@ -1,24 +1,21 @@
 # completion extension to git-completion.bash
 
-_git_auto_squash() { __git_complete_revlist ; }
-_git_bci() { _git_commit; }
-_git_bgrep() { _git_checkout; }
-_git_binb() { _git_checkout; }
-_git_cat() { _git_checkout; }
-
-_git_discard() { _git_log; }
-_git_extract() { _git_log; }
-_git_split() { _git_log; }
-
-_git_wt() { _git_branch; }
-
-_git_ff() { __git_complete_remote_or_refspec; }
-_git_cmv() { _git_log; }
 # _git_file_branch() { _git_checkout "$@"; }
-_git_in() { _git_checkout; }
-_git_not_merged() { _git_checkout; }
-# _git_pop() { _git_checkout; }
-_git_dep_push() { _git_push; }
-# _git_rebase_all() { _git_checkout; }
-_git_remerge() { _git_checkout; }
-_git_squash() { _git_checkout; }
+# _git_pop()         { _git_checkout; }
+# _git_rebase_all()  { _git_checkout; }
+_git_auto_squash()   { __git_complete_revlist ; }
+_git_bci()           { _git_commit; }
+_git_bgrep()         { _git_checkout; }
+_git_binb()          { _git_checkout; }
+_git_cat()           { _git_checkout; }
+_git_cmv()           { _git_log; }
+_git_dep_push()      { _git_push; }
+_git_discard()       { _git_log; }
+_git_extract()       { _git_log; }
+_git_ff()            { __git_complete_remote_or_refspec; }
+_git_in()            { _git_checkout; }
+_git_not_merged()    { _git_checkout; }
+_git_remerge()       { _git_checkout; }
+_git_split()         { _git_log; }
+_git_squash()        { _git_checkout; }
+_git_wt()            { words[1]=checkout; _git_checkout; }
