9347ade6c696915be2f7682b47d4f74adc894e50
drdr xp <drdr.xp@gmail.com>
2018-05-05 18:08:30 +0800

    AM xp/bash-d/plugin/cheatsheet/sheets/vagrant/shrink-disk.sh

---
+++ xp/bash-d/plugin/cheatsheet/sheets/vagrant/shrink-disk.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Step-1
+
+# fillup all disk with zero
+vagrant ssh
+sudo dd if=/dev/zero of=wipefile bs=1024x1024; rm wipefile
+
+
+# Step-2
+
+# virtual box:
+VboxManage clonehd old.vdi new.vdi
+
+
+# vmware:
+vmware-vdiskmanager -d /path/to/main.vmdk
+vmware-vdiskmanager -k /path/to/main.vmdk
