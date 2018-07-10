7582afe7da665c51b9d1b9928f2295dbe1ca5023
drdr xp <xp@baishancloud.com>
2018-07-10 13:14:22 +0800

    M  xp/bash-d/script/subtree-project-update.sh

--- xp/bash-d/script/subtree-project-update.sh
+++ xp/bash-d/script/subtree-project-update.sh
@@ -5,7 +5,7 @@ cd $(git rev-parse --show-toplevel)
 while read path branch remote; do
 
     git subtree split -P "$path" --branch "$branch" --rejoin
-    git push "$remote" git@github.com:drmingdrmer/cheatsheet.git $branch:master
+    git push "$remote" $branch:master
 
 done <<-END
 xp/bash-d/plugin/cheatsheet         cheatsheet         git@github.com:drmingdrmer/cheatsheet.git
