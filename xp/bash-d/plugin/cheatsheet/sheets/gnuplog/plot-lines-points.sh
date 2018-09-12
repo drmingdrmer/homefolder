edb7dd9d2cbd75baa709d028d2a4c382bbe7bbbb
drdr xp <xp@baishancloud.com>
2018-09-12 19:10:05 +0800

    M  xp/bash-d/plugin/cheatsheet/sheets/gnuplog/plot-lines-points.sh

--- xp/bash-d/plugin/cheatsheet/sheets/gnuplog/plot-lines-points.sh
+++ xp/bash-d/plugin/cheatsheet/sheets/gnuplog/plot-lines-points.sh
@@ -7,7 +7,7 @@ set terminal dumb
 set style line 1 lc rgb 'black' pt 5   # square
 set pointtype 2
 
-plot "fn" with lines linestyle 1, \
+plot "fn" with lines linestyle 1 title "blabla", \
      "fn" with points pointtype 2
 END
 } | gnuplot
