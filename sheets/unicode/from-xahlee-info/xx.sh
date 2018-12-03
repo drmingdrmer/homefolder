991db27a1df1d09ccd04f62680eae3ceefc5aec1
drdr xp <drdr.xp@gmail.com>
2018-12-03 16:48:05 +0800

    A  xp/bash-d/plugin/cheatsheet/sheets/unicode/from-xahlee-info/apl.txt

---
+++ sheets/unicode/from-xahlee-info/xx.sh
@@ -0,0 +1,12 @@
+w3m -dump  http://xahlee.info/comp/unicode_common_symbols.html          >common-characters.txt
+w3m -dump  http://xahlee.info/comp/unicode_punctuation_symbols.html     >punctuations.txt
+w3m -dump  http://xahlee.info/comp/unicode_currency_symbols.html        >currency.txt
+w3m -dump  http://xahlee.info/comp/unicode_circled_numbers.html         >circled-numbers-letters.txt
+w3m -dump  http://xahlee.info/comp/unicode_computing_symbols.html       >keyboard-keys.txt
+w3m -dump  http://xahlee.info/comp/unicode_character_representation.html>ascii.txt
+w3m -dump  http://xahlee.info/comp/unicode_arrows.html                  >arrows.txt
+w3m -dump  http://xahlee.info/comp/unicode_matching_brackets.html       >brackets-quotes.txt
+w3m -dump  http://xahlee.info/comp/unicode_math_operators.html          >math.txt
+w3m -dump  http://xahlee.info/comp/unicode_math_font.html               >math-font.txt
+w3m -dump  http://xahlee.info/comp/unicode_greek_alphabet.html          >greek-alphabet.txt
+w3m -dump  http://xahlee.info/comp/unicode_apl_symbols.html             >apl.txt
