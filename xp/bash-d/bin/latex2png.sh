5234ba408b19bc1bd115e33d496b850711eb05ad
drdr xp <xp@baishancloud.com>
2019-01-04 20:29:40 +0800

    M  xp/bash-d/bin/latex2png.sh

--- xp/bash-d/bin/latex2png.sh
+++ xp/bash-d/bin/latex2png.sh
@@ -5,9 +5,14 @@ pdf="${fn%.*}.pdf"
 png="${fn%.*}.png"
 cropped="/tmp/x.pdf"
 
+# latex to pdf
 pdflatex "$fn"
+
+# crop pdf to minimal size
 pdfcrop "$pdf" "$cropped"
 
+# convert pdf to png, no bg
 convert -density 200 "$cropped" -quality 50 "$png"
 
+# another way to convert, white bg
 # pdftoppm "$cropped" | pnmtopng > "$png"
