0b81c2d4767be370682d77224ca00f6b01bb8411
drdr xp <xp@baishancloud.com>
2019-01-05 18:25:17 +0800

    M  xp/bash-d/bin/latex2png.sh

--- xp/bash-d/bin/latex2png.sh
+++ xp/bash-d/bin/latex2png.sh
@@ -16,3 +16,8 @@ convert -density 200 "$cropped" -quality 50 "$png"
 
 # another way to convert, white bg
 # pdftoppm "$cropped" | pnmtopng > "$png"
+
+
+rm "$cropped" "$pdf" \
+    "${pdf%.*}.log" \
+    "${pdf%.*}.aux"
