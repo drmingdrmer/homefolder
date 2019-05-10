1f4bfd6c93860b59b3ab6146a88d01d177972f8f
drdr xp <drdr.xp@gmail.com>
2019-05-11 02:44:27 +0800

    M  xp/bash-d/rc/bash-plugin-rc/homebrew.sh

--- xp/bash-d/rc/bash-plugin-rc/homebrew.sh
+++ xp/bash-d/rc/bash-plugin-rc/homebrew.sh
@@ -1,3 +1,6 @@
 # my personal github token for homebrew
 # See: https://github.com/settings/tokens
 export HOMEBREW_GITHUB_API_TOKEN=664586d89dd5a42239308b95ae45fd24da1f91bc
+
+# do not update, very slow
+export HOMEBREW_NO_AUTO_UPDATE=1
