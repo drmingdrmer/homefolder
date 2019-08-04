e355c199c3fd54fe820b36528c5c18edf0f6c708
drdr xp <xp@baishancloud.com>
2019-08-04 15:30:48 +0800

    M  xp/bash-d/plugin/cheatsheet/sheets/go/disassemble.sh

--- xp/bash-d/plugin/cheatsheet/sheets/go/disassemble.sh
+++ xp/bash-d/plugin/cheatsheet/sheets/go/disassemble.sh
@@ -2,3 +2,8 @@ go tool objdump -S bin
 go tool objdump bin
 
 # https://stackoverflow.com/questions/23789951/easy-to-read-golang-assembly-output
+
+
+# with gopath:
+#   go tool compile -I $GOPATH/pkg/darwin_amd64 -S x.go
+go tool compile -S x.go
