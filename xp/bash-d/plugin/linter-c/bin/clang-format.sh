89e510b8eabf5a37659a2f9737df715f6d0d7287
drdr xp <drdr.xp@gmail.com>
2021-02-23 14:58:40 +0800

    A  xp/bash-d/bin/gclone

---
+++ xp/bash-d/plugin/linter-c/bin/clang-format.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+ROOT=`dirname "$0"`
+ROOT=`cd "$ROOT"; pwd`
+
+export DORIS_HOME=`cd "${ROOT}/.."; pwd`
+
+CLANG_FORMAT=${CLANG_FORMAT_BINARY:=$(which clang-format)}
+
+python3 ${DORIS_HOME}/build-support/run_clang_format.py --clang_format_binary="${CLANG_FORMAT}" --fix --source_dirs="${DORIS_HOME}/be/src","${DORIS_HOME}/be/test"
+
+
