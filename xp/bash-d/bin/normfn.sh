a5bd32c691d2d636d607337bbfa0b8edabc8a78c
张炎泼 <drdr.xp@gmail.com>
2023-12-12 10:32:16 +0800

    M  xp/bash-d/bin/normfn.sh

--- xp/bash-d/bin/normfn.sh
+++ xp/bash-d/bin/normfn.sh
@@ -6,6 +6,7 @@ usage()
     echo "convert all arguments to valid all-lower normalized file name"
     echo ' - replace non word char with -'
     echo ' - convert to lower case'
+}
 
 normalized=$(echo "$*" | awk '{
     gsub(/[^a-zA-Z0-9]/, "-", $0);
