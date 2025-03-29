d5b9a76dfe9564dcef685ef3bce82324b626cdc0
Zhang Yanpo <drdr.xp@gmail.com>
2025-03-29 10:59:39 +0800

    M  xp/plugin/markdown-util/bin/md-remove-link.py

--- xp/plugin/markdown-util/bin/md-remove-link.py
+++ xp/plugin/markdown-util/bin/md-remove-link.py
@@ -5,25 +5,35 @@ import sys
 import re
 import argparse
 
-def remove_links(content):
-    # Remove inline links [text](url) but not images ![alt](url)
-    content = re.sub(r'(?<!!)\[([^\]]+)\]\([^)]+\)', r'\1', content)
-    # Remove reference links [text][ref] but not images ![alt][ref]
-    content = re.sub(r'(?<!!)\[([^\]]+)\]\[[^\]]*\]', r'\1', content)
+# Predefined regex patterns
+INLINE_LINK_PATTERN = r'(?<!!)\[([^\]]+)\]\([^)]+\)'
+REFERENCE_LINK_PATTERN = r'(?<!!)\[([^\]]+)\]\[[^\]]*\]'
+REFERENCE_DEF_PATTERN = r'^\[[^\]]+\]:\s*http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+def remove_links(content, remove_text=False):
+    # Determine replacement pattern based on remove_text
+    replacement = r'' if remove_text else r'\1'
+    
+    # Remove inline links [text](url)
+    content = re.sub(INLINE_LINK_PATTERN, replacement, content)
+    # Remove reference links [text][ref]
+    content = re.sub(REFERENCE_LINK_PATTERN, replacement, content)
+    
     # Remove reference definitions [ref]: url
-    content = re.sub(r'^\[[^\]]+\]:\s*http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', content, flags=re.MULTILINE)
+    content = re.sub(REFERENCE_DEF_PATTERN, '', content, flags=re.MULTILINE)
     return content
 
 def main():
     parser = argparse.ArgumentParser(description='Remove links from markdown file')
     parser.add_argument('file', help='Input markdown file')
+    parser.add_argument('-r', '--remove-text', action='store_true', help='Remove link text completely instead of keeping it')
     args = parser.parse_args()
     
     try:
         with open(args.file, 'r', encoding='utf-8') as f:
             content = f.read()
         
-        result = remove_links(content)
+        result = remove_links(content, args.remove_text)
         print(result, end='')
         
     except FileNotFoundError:
